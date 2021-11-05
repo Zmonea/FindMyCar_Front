e33ee2a46888a59b6766cae6f877d035a2823fbe
zmonea <Zac.j.monea@gmail.com>
2021-11-05 13:08:26 -0400

    Updated with CarList and Car Cars as well as improved delete and edit functionality

---
+++ src/components/carList.js
@@ -0,0 +1,68 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CarCard from './carCard';
+
+
+const CarList = () => {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/cars')
+            .then((response) => {
+                setCars(response.data)
+            })
+    }, [])
+
+    const handleDelete = (carData) => {
+        axios
+            .delete(`http://localhost:3000/cars/${carData._id}`)
+            .then(() => {
+                axios
+                    .get('http://localhost:3000/cars')
+                    .then((response) => {
+                        setCars(response.data)
+                    })
+            })
+    }
+
+    const editCar = (carData) => {
+        axios
+            .put(
+                `http://localhost:3000/cars/${carData._id}`,
+                {
+                    make: carData.make,
+                    model: carData.model,
+                    year: carData.year,
+                    color: carData.color,
+                    image: carData.image,
+                    price: carData.price,
+                }
+            )
+            .then(() => {
+                axios
+                    .get('http://localhost:3000/cars')
+                    .then((response) => {
+                        setCars(response.data)
+                    })
+            })
+    }
+    return (
+        <section>
+            <h2 className="textAlignCenter">Cars</h2>
+            <ul className="flexCarCards">
+                {
+                    cars.map((car) => {
+                        return <CarCard car={car} handleDelete={handleDelete} editCar={editCar} />
+                    })
+                }
+
+            </ul>
+        </section>
+
+    );
+
+}
+
+export default CarList;
