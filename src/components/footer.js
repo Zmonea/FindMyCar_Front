336b28657f792b1a28635632a8c7d4faf9c8bd35
Jimmy Nguyen <gymmewin@Jimmys-MacBook-Pro.local>
2021-11-05 14:40:25 -0400

    fixed broken linkedIn icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
       <footer>
          <div id="Jimmy">
             <p>© GYMMEWIN</p>
-            <a href="https://www.linkedin.com/in/gymmewin/"> <img src="https://cdn-icons.flaticon.com/png/128/3536/premium/3536505.png?token=exp=1636056917~hmac=62d37edb5ce8aa0b45db0cf12aa5e2c5" alt="LinkedIn" className="icons"/></a>
+            <a href="https://www.linkedin.com/in/gymmewin/"> <img src="https://cdn-icons.flaticon.com/png/128/3256/premium/3256016.png?token=exp=1636138437~hmac=a88448de3fedd594008648f16bafdedb" alt="LinkedIn" className="icons"/></a>
 
             <a href="https://github.com/gymmewin"> <img src="https://cdn-icons-png.flaticon.com/128/270/270798.png" alt="Github" className="icons"/> </a>
          </div>
 
          <div id="Zach">
             <p>© ZACH MONEA </p>
-            <a href="https://www.linkedin.com/in/zachary-monea/"><img src="https://cdn-icons.flaticon.com/png/128/3536/premium/3536505.png?token=exp=1636056917~hmac=62d37edb5ce8aa0b45db0cf12aa5e2c5" alt="LinkedIn" className="icons"/></a>
+            <a href="https://www.linkedin.com/in/zachary-monea/"><img src="https://cdn-icons.flaticon.com/png/128/3256/premium/3256016.png?token=exp=1636138437~hmac=a88448de3fedd594008648f16bafdedb" alt="LinkedIn" className="icons"/></a>
 
             <a href="https://github.com/zmonea"> <img src="https://cdn-icons-png.flaticon.com/128/270/270798.png" alt="Github" className="icons"/> </a>
          </div>
